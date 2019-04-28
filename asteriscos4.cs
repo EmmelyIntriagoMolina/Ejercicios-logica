a085a5b9728c4dbf79f1b1b0c301ad2bb6e626df
EmmelyIntriagoMolina <50086890+EmmelyIntriagoMolina@users.noreply.github.com>
2019-04-28 18:06:43 -0500

    Add files via upload

---
+++ asteriscos4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asteriscos4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0, j=0, l=0;
+            Console.WriteLine("Ingrece número de escalones para la pirámide: ");
+            int numero = Convert.ToInt16(Console.ReadLine());
+
+            for (i=0; i<numero; i++)
+            {
+                for (j = i; j<numero; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (l = 0; l != i; l++)
+                {
+                    Console.Write("**");
+                }
+                Console.WriteLine("*");
+            }
+            Console.ReadKey();
+        }
+    }
+}
