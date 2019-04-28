6edf549afab18fc85696d8a374a11b919c94cd06
EmmelyIntriagoMolina <50086890+EmmelyIntriagoMolina@users.noreply.github.com>
2019-04-28 18:06:22 -0500

    Add files via upload

---
+++ numeros.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador1 = 1, contador2=100 ;
+            do
+            {
+                Console.WriteLine(contador1 + "   " + contador2);
+                contador1 = contador1 + 1;
+                contador2 = contador2 - 1;
+
+            } while (contador1 <= 100 && contador2 >= 1);      
+
+            
+            Console.ReadKey();
+        }
+    }
+}
