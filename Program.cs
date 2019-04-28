b09454097d738f5b6a2c3bb1e220872154e688b1
EmmelyIntriagoMolina <50086890+EmmelyIntriagoMolina@users.noreply.github.com>
2019-04-28 17:55:21 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,30 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-namespace EmmelyIntriagoTerceroA
+namespace Cuadrado
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int mayor = 0;
-
-            Console.WriteLine("Ingrese el número total de elementos para la serie: ");
+            
+            Console.WriteLine("Ingrese el número de asteriscos por lado: ");
             int n = Convert.ToInt16(Console.ReadLine());
 
-            for (int i=1; i<=n; i++)
+            int a, b;
+
+            for(a=0; a <n; a++)
             {
-                Console.WriteLine("Ingrese valor: ");
-                int valor = Convert.ToInt16(Console.ReadLine());
 
-                if (valor>mayor)
+                for (b = 0; b < n; b++)
                 {
-                    mayor = valor;
-                }
-            }
+                    if (a==0||b==0||a==n-1||b==n-1)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
 
-            Console.WriteLine("El número mayor es: " + mayor);
+                }
+                Console.WriteLine(" ");
+            }
             Console.ReadKey();
         }
     }
